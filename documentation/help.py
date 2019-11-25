7b6bc49584cb3363b212723e65b4e09eb2c58ec0
Manuel <mmacedon@uci.edu>
2019-11-25 15:35:04 -0800

    Created User Input Fields
    Provided help and usage information and friendly user interface.

---
+++ documentation/help.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+def print_help():
+    print("""Geometry Problem Solver: Help Section
+                The two problems that the can be solved:
+                    - Circle and Triangle
+                    - Line and Triangle
+                The Program is to solve the two images satisfying
+                at most 3 geometric predicates.
+                The following predicates are applicable to the two
+                problems presented in the images displayed.
+                Usage is listed along with each predicate.
+                Predicate & Usage:
+                    >> set_parallel(name1, name2)
+                        Ex: >> set_parallel(s2, s6)
+                    >> set_perpendicular(name1, name2)
+                        Ex: >> set_perpendicular(s8, s9)
+                    >> set_equal(name1, name2, fraction)
+                        Ex: >> set_fraction(s8,s9, 1/4 )
+                    >> set_sum_value(name1, name2, sum)
+                        Ex: >> set_sum_value(a7, a8, 90)
+                    >> set_congruent(name1, name2)
+                        Ex: set_congruent(ar1, ar7)
+                    >> set_tan(name1, name2)
+                        Ex: set_tan(s2,s4)
+                Inputs can be atmost 3 geomertic predicates.
+                    >> set_parallel(name1, name2), set_perpendicular(name3, name4), set_fraction(name5, name6, 1/n)
+                Prepend the name of the problem to solve:
+                    >> Circle and Triangle set_parallel(name1, name2), set_perpendicular(name3, name4), set_fraction(name5, name6, 1/n)
+          """)
--- geometrysolver.py
+++ geometrysolver.py
@@ -1,29 +1,71 @@
 #!/usr/bin/env python
+import documentation.license
+import documentation.help
+
+try:
+    from PIL import Image
+except:
+    print("""Import Error: Module Image required for
+              GeometryProblemSolver not found.
+              Run: pip install Image
+              Restart Code.
+                """)
+    exit(1)
+
+def display_images():
+
+    image = Image.open('./img/Circle and Triangle 14.JPG')
+    image_2 = Image.open('./img/Line and Triangle 1.JPG')
+    if ( image == None or image_2 == None ):
+        return -1
+    else:
+        image.show()
+        image_2.show()
+    return 0
 
 def main():
+
     print("Welcome to Geometry Problem Solver")
     print("""
             For Usage of Program: Input Three or More Predicates in the Following
-            Format: [predicate name](parameter_1, parameter_2,..., parameter_n)
+            Format: [problem] [predicate name](parameter_1, parameter_2,..., parameter_n)
+            The Problems are:
+                - Circle and Triangle
+                - Line and Triangle
             Use the images presented to choose the parameters.
+            Allow the images to load.
             For Help: -help
             For License: -license
             For Exiting: -quit
-            Code Can Be Found on GitHub: https://github.com/mmacedon/GeometryProblemSolver
+            GitHub: https://github.com/mmacedon/GeometryProblemSolver
         """)
+
+    #Display the Images to the User
+    success = display_images()
+    if ( success == -1 ):
+        print("""Image Error: Make sure that the two images 'Circle and Triangle 14.JPG'
+                    and 'Line and Triangle 1.JPG' are in the directory ./GeometryProblemSolver/img/.
+                    For redownloading the images, visit the Github link. Place within directory and
+                    restart code.""")
+        exit(1)
 
     while ( True ):
         user_input = input(">> ")
         if ( user_input == "-quit" ):
-            print("Thank you for using the Geometry Solver\n")
             break
         elif ( user_input == "-license"):
-            license.printlicense()
+            license.print_license()
         elif ( user_input == "-help"):
-            help.printhelp()
+            help.print_help()
+        elif("circle" in user_input or "Circle" in user_input):
+            solver_problem_1(user_input)
+        elif ("Line" in user_input or "line" in user_input):
+            solver_problem_2(user_input)
         else:
-            solver(user_input)
+            print("Invalid Input\n")
+            print("Enter -help for help using the solver.")
 
+    print("Thank you for using the Geometry Solver\n")
     return 0
 
 if __name__ == '__main__':
